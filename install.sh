350a1e9e26218b4c3daad1f94d5b5e0138f9782b
𝕋𝕖𝕛∆𝕤 𝕡𝕣𝕠 𝕘∆𝕞𝕚𝕟𝕘 ¹⁷¹³ <tejassgupta1327@gmail.com>
2026-02-19 18:49:02 +0530

    Update installation messages in install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,12 @@
 
 set -e
 
+REPO="https://raw.githubusercontent.com/tejasprogaming/minifetch/main/minifetch"
+
 echo "Installing MiniFetch..."
+
+# Download
+curl -L "$REPO" -o minifetch
 
 # Make executable
 chmod +x minifetch
@@ -10,5 +15,5 @@ chmod +x minifetch
 # Move to system path
 sudo mv minifetch /usr/local/bin/minifetch
 
-echo "Done ✅"
+echo "Installation complete ✅"
 echo "Run: minifetch"
